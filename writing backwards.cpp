75fd79c1f7ef513189520e99b5ac433d3f9831d7
Andra Popovici <ioana.popovici95@yahoo.com>
2020-03-20 01:42:04 +0000

    Add files via upload
    string as array - input
    function definition added

---
+++ writing backwards.cpp
@@ -0,0 +1,22 @@
+
+#include <headerforwritingbw.h>
+
+using namespace std;
+
+//maybe add some graphics
+
+// Main purpose of the application
+// is to have a string as an input from the user and the output will represent the text written backwards
+
+
+int main()  {
+
+cin>>"Enter your text here: ">>user_input;
+
+double int lengthoftext
+
+for (int i = user_input[lengthoftext] {
+    
+}
+    return 0;
+}
